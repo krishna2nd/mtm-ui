e6bf196dbd8adc46cd5eeb8540d6482966fd7595
Chetan Wadhwa <chetan.wadhwa@microsoft.com>
2019-11-19 10:45:27 +0530

    List in variable component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import VariableComponent from "./VariableComponent";
 
 export const Router: React.FC = (props) => (
   <BrowserRouter>
     <Switch>
       <Route path="/triggers" component={() => <div>Triggers</div>}></Route>
-      <Route path="/variables" component={() => <div>Variables</div>}></Route>
+      <Route path="/variables" component={() => <VariableComponent/>}></Route>
       <Route path="/" exact component={() => <div>Tags</div>}></Route>
       <Redirect to="/" />
     </Switch>
